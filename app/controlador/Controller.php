f6aba9dcf9002523bbcfee216cf7b820c6ec479b
Gornko <pablogorn@gmail.com>
2025-06-03 19:12:40 +0200

    retoques info usuario

--- app/controlador/Controller.php
+++ app/controlador/Controller.php
@@ -125,6 +125,7 @@ class Controller
 
                             $_SESSION['idUser'] = $usuario['id'];
                             $_SESSION['nombreUsuario'] = $usuario['username'];
+                            $_SESSION['photo']=$usuario['profile_image'];
                             if ($usuario['role'] == 'admin') {
                                 $_SESSION['nivel'] = 2;
                             } else {
--- web/templates/layout.php
+++ web/templates/layout.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo Config::$vista_css; ?>">
+    
 
 
     <title>Document</title>
@@ -14,6 +15,11 @@
     <header>
         <!-- Generar dinamicamente el menu si esta logueado-->
         <?php if (isset($_SESSION['nivel']) && $_SESSION['nivel'] > 0): ?>
+            <div class="infoUser">
+            <p><?php echo $_SESSION['nombreUsuario'] ?></p>
+            <img class="userImg" src="/dwes/Extraordinaria/evaluableextraordinaria/web/images/users/<?php echo $_SESSION['photo'] ?>" alt="No">
+            </div>
+            
             <a href="index.php?ctl=logout" class="logout-button">Cerrar sesión</a>
         <?php endif; ?>
     </header>
